fe066db2744fd02d8570e111424b21554a579f02
Deepesh Choudhary <dcdeepesh7@gmail.com>
2020-07-25 01:34:41 +0530

    Refactor Main.js
    Change static function to a self calling function.
    
    Init grammarly only after local initialization is done.

--- Main.js
+++ Main.js
@@ -2,32 +2,28 @@ const DOCXManager = require('./DOCXManager.js');
 const Grammarly = require('./Grammarly.js');
 const Util = require('./Util.js');
 
-class Main {
-    static async foo() {
-        var filePath = process.argv[2];
-        if (filePath == undefined || filePath.trim() == '')
-            return;
+(async function() {
+    var filePath = process.argv[2];
+    if (filePath == undefined || filePath.trim() == '')
+        return;
 
-        Util.log("Initializing...");
-        await Grammarly.init();
-        await DOCXManager.init(filePath);
-        DOCXManager.readParagraphs();
-        
-        Util.log("Checking...");
-        let res = await Grammarly.check(DOCXManager.toString());
+    Util.log("Initializing...");
+    await DOCXManager.init(filePath);
+    DOCXManager.readParagraphs();
+    await Grammarly.init();
 
-        Util.log(`Fixing ${res.length} issues...`);
-        DOCXManager.replaceAll(res);
+    Util.log("Checking...");
+    let res = await Grammarly.check(DOCXManager.toString());
 
-        Util.log("Writing output...");
-        await DOCXManager.updateAndPackage();
+    Util.log(`Fixing ${res.length} issues...`);
+    DOCXManager.replaceAll(res);
 
-        Util.log("Finalizing...");
-        Grammarly.shutdown();
-        DOCXManager.cleanUp();
+    Util.log("Writing output...");
+    await DOCXManager.updateAndPackage();
 
-        Util.log("-- Correction completed successfully --");
-    }
-}
+    Util.log("Finalizing...");
+    Grammarly.shutdown();
+    DOCXManager.cleanUp();
 
-Main.foo().catch(error => Util.log(error));
+    Util.log("-- Correction completed successfully --");
+})();
